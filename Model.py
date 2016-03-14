b08380958cb2eeda9022be6535363850b6d57b37
gloups01 <gloria_wanet@ymail.com>
2016-03-14 10:42:03 +0100

    Create Model.py

---
+++ Model.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import sqlite3
+import sys
+from View import *
+
+class Model(View):
+  	def __init__(self):
+  		self.createDatabase()
+  		view.
+
+  def createDatabase(self):
+    #création et remplissage de la database
+    
+    db = sqlite3.connect('DataBase.db')
+    cursor = db.cursor()
+    
+    cursor.execute("""
+    DROP TABLE contacts
+    """)
+
+    cursor.execute("""
+    CREATE TABLE contacts(
+        name TEXT,
+        lastName TEXT,
+        number INT,
+        adress TEXT
+    );
+    """)
+    
+    cursor.execute("""
+    INSERT INTO contacts(name, lastName) VALUES(?,?)""",("Machin", "tac",))
+
+    cursor.execute("""
+    INSERT INTO contacts(name, lastName) VALUES(?,?)""",("Bidule", "tic",))
+
+    cursor.execute("""
+    INSERT INTO contacts(name, lastName) VALUES(?,?)""",("Monsieur", "mme",))
+
+    cursor.execute("""
+    INSERT INTO contacts(name, lastName) VALUES(?,?)""",("Maman", "truc",))
+
+    cursor.execute("""
+    INSERT INTO contacts(name, lastName) VALUES(?,?)""",("Travail", "bueno",))
+
+    cursor.execute("""
+    INSERT INTO contacts(name, lastName) VALUES(?,?)""",("Ecole", "kinder",))
+
+    cursor.execute("""
+    SELECT name, lastName FROM contacts""")
+    person = cursor.fetchone()
+    db.commit()
